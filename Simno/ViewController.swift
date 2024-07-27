6fdf6ca2323e12ac52d1597311e1259ff611b786
Vlad Gorelov <vsg1993@yandex.ru>
2024-07-27 13:05:29 +0300

    Initial commit

---
+++ Simno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Simno
+//
+//  Created by Владислав Горелов on 27.07.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
